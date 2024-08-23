2037b625f59fa716a6340a3e76ebc0f1b43094dc
jesse-gauthier <jessegauthier2@gmail.com>
2024-08-23 10:51:41 -0400

    adds flex to footer service and company links for mobile

--- src/components/footer/FooterComp.vue
+++ src/components/footer/FooterComp.vue
@@ -3,7 +3,7 @@
 		class="footer p-10 bg-[#2b3d4f] flex flex-wrap justify-center gap-12 text-white min-h-[10vh]"
 		role="contentinfo"
 	>
-		<div class="max-w-[300px]" data-aos="fade-up">
+		<div class="max-w-[300px] flex flex-wrap" data-aos="fade-up">
 			<h4 class="text-2xl">Our Mission</h4>
 			<p>
 				Our mission is to craft high-quality websites designed to empower
@@ -14,67 +14,69 @@
 				<a href="mailto:Jesse@ottawawebmasters.ca">Jesse@ottawawebmasters.ca</a>
 			</div>
 		</div>
-		<!-- Services -->
-		<nav aria-label="Services" data-aos="fade-up">
-			<h4 class="text-2xl">Services</h4>
-			<ul class="">
-				<li>
-					<router-link class="link link-hover" to="/services"
-						>Web Development</router-link
-					>
-				</li>
-				<li>
-					<router-link class="link link-hover" to="/shopify"
-						>Shopify</router-link
-					>
-				</li>
-				<li>
-					<router-link class="link link-hover" to="/wordpress"
-						>WordPress</router-link
-					>
-				</li>
-				<li>
-					<router-link class="link link-hover" to="/services"
-						>UX Design</router-link
-					>
-				</li>
-				<li>
-					<router-link class="link link-hover" to="/seo">SEO</router-link>
-				</li>
-			</ul>
-		</nav>
-		<!-- Company -->
-		<nav data-aos="fade-up" aria-label="Company">
-			<h4 class="text-2xl">Company</h4>
-			<ul>
-				<li><a href="/about-us" class="link link-hover">About us</a></li>
-				<li>
-					<router-link class="link link-hover" to="/contact"
-						>Contact</router-link
-					>
-				</li>
-				<li>
-					<router-link class="link link-hover" to="/ourwork"
-						>Portfolio</router-link
-					>
-				</li>
-				<li>
-					<router-link class="link link-hover" to="/privacy-policy"
-						>Privacy Policy</router-link
-					>
-				</li>
-				<li>
-					<router-link class="link link-hover" to="/freeseo"
-						>Free SEO Audit</router-link
-					>
-				</li>
-				<li>
-					<router-link class="link link-hover" to="/freeseo"
-						>Free SEO Check List</router-link
-					>
-				</li>
-			</ul>
-		</nav>
+		<div class="flex flex-wrap gap-12">
+			<!-- Services -->
+			<nav aria-label="Services" data-aos="fade-up">
+				<h4 class="text-2xl">Services</h4>
+				<ul class="">
+					<li>
+						<router-link class="link link-hover" to="/services"
+							>Web Development</router-link
+						>
+					</li>
+					<li>
+						<router-link class="link link-hover" to="/shopify"
+							>Shopify</router-link
+						>
+					</li>
+					<li>
+						<router-link class="link link-hover" to="/wordpress"
+							>WordPress</router-link
+						>
+					</li>
+					<li>
+						<router-link class="link link-hover" to="/services"
+							>UX Design</router-link
+						>
+					</li>
+					<li>
+						<router-link class="link link-hover" to="/seo">SEO</router-link>
+					</li>
+				</ul>
+			</nav>
+			<!-- Company -->
+			<nav data-aos="fade-up" aria-label="Company">
+				<h4 class="text-2xl">Company</h4>
+				<ul>
+					<li><a href="/about-us" class="link link-hover">About us</a></li>
+					<li>
+						<router-link class="link link-hover" to="/contact"
+							>Contact</router-link
+						>
+					</li>
+					<li>
+						<router-link class="link link-hover" to="/ourwork"
+							>Portfolio</router-link
+						>
+					</li>
+					<li>
+						<router-link class="link link-hover" to="/privacy-policy"
+							>Privacy Policy</router-link
+						>
+					</li>
+					<li>
+						<router-link class="link link-hover" to="/freeseo"
+							>Free SEO Audit</router-link
+						>
+					</li>
+					<li>
+						<router-link class="link link-hover" to="/freeseo"
+							>Free SEO Check List</router-link
+						>
+					</li>
+				</ul>
+			</nav>
+		</div>
 	</footer>
 </template>
 
