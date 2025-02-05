7f2a0f914a78ef0326d342a34de41ebf1c62c68b
Jesse Gauthier <jessegauthier2@gmail.com>
2025-02-05 14:01:18 -0500

    chore: update vite-plugin-vue-devtools to version 7.7.1 and refactor HeaderComp.vue for improved structure and styling

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), sentryVitePlugin({
+    plugins: [vue(), VueDevTools(), sentryVitePlugin({
         org: "ottawa-web-masters",
         project: "javascript-vue"
     })],
